af5edded324a505414adc58a10d35ce57ca07acb
kashyapanjali <anjalikashyap9608@gmail.com>
2024-03-24 00:59:23 +0530

    test

--- Graph/BFsTraverse.java
+++ Graph/BFsTraverse.java
@@ -43,7 +43,7 @@ public class BFsTraverse {
         graph[6].add(new Edge(6,5,1));
     }
 
-    public static void bfs(ArrayList<Edge>graph[]){
+    public static void bfs(ArrayList<Edge>graph[]){//(V+E)
         Queue<Integer>q=new LinkedList<>();
         boolean vis[]=new boolean[graph.length];
         q.add(0); //src=0
